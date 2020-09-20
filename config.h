e648f2d41e0f427f6db16bcd676af70e9dd883af
Michael Kelly <michaelkelly322@gmail.com>
2020-09-20 00:19:37 -0400

    Add .local/src related commands

--- config.h
+++ config.h
@@ -53,19 +53,22 @@ static const Layout layouts[] = {
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+#define SHCMD(cmd) { .v = (const char*[]){ "/usr/bin/zsh", "-c", cmd, NULL } }
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *surfcmd[]  = { "surf", "https://duckduckgo.com", NULL };
+static const char *srcmd[]  = { "/home/michael/.local/src/scripts/src_term.sh", NULL };
+static const char *calcmd[]  = { "st", "-d", "/home/michael/.local/src/calendly", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = surfcmd } },
+	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = srcmd } },
+	{ MODKEY|ShiftMask,             XK_w,      spawn,          {.v = calcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
